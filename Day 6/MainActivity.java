504144ced13eac65a3bcc537aad175fb9cab31f9
Aniket Bhanderi <43649759+Aniketbhanderi@users.noreply.github.com>
2021-06-14 00:33:31 +0530

    Create MainActivity.java

---
+++ Day 6/MainActivity.java
@@ -0,0 +1,24 @@
+package com.example.myapplication2;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class    MainActivity extends AppCompatActivity {
+
+    Button btnSubmit;
+    EditText n1;
+    EditText n2;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+
+
+
+}
